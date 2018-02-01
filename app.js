4c1b9cbc9577731f14db49230a854ed8e00d5fd6
Florian <acounts@florianbouchet.fr>
2018-02-01 15:40:54 +0100

    add routes

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
+var dbClient = require('mariasql');
+
+var db = new dbClient();
+db.connect
+({
+    host: '127.0.0.1',
+    user: 'homestead',
+    password: 'secret'
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+//    resSql = db.query('select * from deliveries');
+    res.send('resSql');
+});
+
+app.post('/:id', function (req, res) {
+//    db.query('insert ...');
+    res.send(req.params.id);
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App is running.');
 });
